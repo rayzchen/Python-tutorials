92010032891ff58684126976832e75f7e0a098fe
rayzchen <54078384+rayzchen@users.noreply.github.com>
2020-06-29 11:21:25 +0100

    Update tutorial1.py

---
+++ tutorial1.py
@@ -0,0 +1,24 @@
+# Printing
+
+print("Hello World!")
+
+# Variables
+
+x = "Hi!"
+print(x)
+
+# Data types
+
+x = 1 # Integer
+y = 1.0 # Float
+z = "1" # String
+
+# Concatenation
+
+a = z + "h"
+
+# Converting
+
+b = a + str(11)
+c = 10 + int("11")
+d = 2.0 + float("4.6")
